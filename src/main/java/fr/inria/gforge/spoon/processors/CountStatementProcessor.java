4d68dc7d10e371669fb77a038929b4f6b6a507a8
Gerard Paligot <gerard.paligot@gmail.com>
2014-10-30 18:13:12 +0100

    Improves CountStatementProcessor to append results in file

--- src/main/java/fr/inria/gforge/spoon/processors/CountStatementProcessor.java
+++ src/main/java/fr/inria/gforge/spoon/processors/CountStatementProcessor.java
@@ -4,7 +4,9 @@ import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtStatement;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.UUID;
 
 /**
  * Created by gerard on 21/10/2014.
@@ -21,22 +23,22 @@ public class CountStatementProcessor extends AbstractProcessor<CtStatement> {
 	@Override
 	public void processingDone() {
 		super.processingDone();
+
 		PrintWriter writer = null;
 		try {
 			// Creates output file for performance.
-			final String directories =
-					"target" + File.separator + "spoon-maven-plugin";
-			final String file =
-					directories + File.separator + "spoon-nb-statement.txt";
-			final File directoryForPerformanceFile = new File(directories);
-			final File nbStatementFile = new File(file);
-			if (!directoryForPerformanceFile.exists()) {
-				directoryForPerformanceFile.mkdirs();
+			final String performancePath =
+					"target" + File.separator + "spoon-maven-plugin"
+							+ File.separator + "spoon-nb-statement.txt";
+			final File performanceFile = new File(performancePath);
+
+			if (!performanceFile.getParentFile().exists()) {
+				performanceFile.getParentFile().mkdirs();
 			}
-			if (!nbStatementFile.exists()) {
-				nbStatementFile.createNewFile();
+			if (!performanceFile.exists()) {
+				performanceFile.createNewFile();
 			}
-			writer = new PrintWriter(nbStatementFile, "UTF-8");
+			writer = new PrintWriter(new FileWriter(performanceFile, true));
 
 			// Saves number of statement.
 			writer.println(count);
